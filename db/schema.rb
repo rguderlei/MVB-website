7664650c5a9bbc5e7dd0359df2cf0635179c75bc
Ralph Guderlei <ralph.guderlei@gmail.com>
2011-04-14 22:14:42 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110414193349) do
+ActiveRecord::Schema.define(:version => 20110414195647) do
+
+  create_table "infos", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pages", :force => true do |t|
     t.string   "name"

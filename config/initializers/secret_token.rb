541ba515389e86628eb4e015b595aa39e6f12f29
Ralph Guderlei <ralph.guderlei@gmail.com>
2011-04-14 21:27:00 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvb::Application.config.secret_token = 'fc8e1436c80cbbc72731d2769a74aae303afbfb171614ae583bf6e835d5444bf0939f6b9cf9f57bfe9749ee8b6062dc429f3f3c394e15e835130cfef0e1ca94f'

1683bd313dbb013032221d3bd43152670a176577
Ralph Guderlei <ralph.guderlei@gmail.com>
2011-05-19 22:45:44 +0200

    fix encoding in german locale, fix syntax issues

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,6 @@
 class Gallery < ActiveRecord::Base
     attr_accessible :name, :context_date
     has_many :photos, :dependent => :destroy
+
+    accepts_nested_attributes_for :photos
 end
--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,8 @@ module Mvb
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding = "UTF-8"
+    Encoding.default_external = 'UTF-8' 
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]

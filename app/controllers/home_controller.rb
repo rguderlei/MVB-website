8bd2318273188af57c3946eed0b9b45757dd444c
Ralph Guderlei <ralph.guderlei@gmail.com>
2011-07-17 23:30:28 +0200

    sketch data provisioning for calendar bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    today = Date.today
+    start_date = Date.new(today.year, today.month, 1)
+    end_date = Date.new(today.year, today.month + 1, 1) -1
+    @events = Event.where(:begin=> start_date..end_date)
   end
 
 end

fecad1721c5e1fc60bfa7f5bda543a0113d3b870
Ralph Guderlei <ralph.guderlei@gmail.com>
2011-07-19 21:37:57 +0200

    finish simple event calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
   def index
     today = Date.today
-    start_date = Date.new(today.year, today.month, 1)
-    end_date = Date.new(today.year, today.month + 1, 1) -1
-    @events = Event.where(:begin=> start_date..end_date)
+    @start_date = Date.new(today.year, today.month, 1)
+    @end_date = Date.new(today.year, today.month + 1, 1) -1
+    @events = Event.where(:begin=> @start_date..@end_date)
+
+    render :locals=>{:events=>@events, :start_date=>@start_date, :end_date=>@end_date}
   end
 
 end

a52af5f35dc474507075533694d0f3dcabd5f1a7
Ralph Guderlei <ralph.guderlei@gmail.com>
2012-01-06 22:51:20 +0100

    Unterschiedliche Farben für Kalendereinträge der Orchester

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   has_event_calendar  :start_at_field  => 'begin', :end_at_field => 'end'
+
+  def color
+    return (orchestra=='Stadtkapelle')? 'blue': 'green';
+  end
 end

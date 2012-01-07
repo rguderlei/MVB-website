ef2a4bbed91915500de77fede292863ee2a6e9b5
Ralph Guderlei <ralph.guderlei@gmail.com>
2012-01-07 22:11:46 +0100

    order next events on start page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     today = Date.today
     @start_date = Date.today - 1 # Date.new(today.year, today.month, 1)
     @end_date = Date.today + 30  # Date.new(today.year, today.month + 1, 1) -1
-    @monthly_events = Event.where(:begin=> @start_date..@end_date)
+    @monthly_events = Event.where(:begin=> @start_date..@end_date).order("begin")
 
     @latest_info = Info.order("created_at DESC").first
 

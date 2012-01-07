69f057b43374ba4e914863726a834b223220c42c
Ralph Guderlei <ralph.guderlei@gmail.com>
2012-01-07 22:36:10 +0100

    layout semistatic pages, now with page title

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
   has_event_calendar  :start_at_field  => 'begin', :end_at_field => 'end'
   validates_presence_of :begin, :end, :title, :orchestra
   def color
-    return (orchestra=='Stadtkapelle')? 'blue': (orchestra=='Sinfonieorchester')? 'green': 'yellow' ;
+    return (orchestra=='Stadtkapelle')? 'blue': (orchestra=='Sinfonieorchester')? 'green': 'orange' ;
   end
 end

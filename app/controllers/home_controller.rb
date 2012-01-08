c5ab173d67856c368cbf32e92def5df52300332b
Ralph Guderlei <ralph.guderlei@gmail.com>
2012-01-08 11:48:16 +0100

    show events starting from today on the start page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,16 @@
 class HomeController < ApplicationController
   def index
     today = Date.today
-    @start_date = Date.today - 1 # Date.new(today.year, today.month, 1)
+    @start_date = Date.today  # Date.new(today.year, today.month, 1)
     @end_date = Date.today + 30  # Date.new(today.year, today.month + 1, 1) -1
     @monthly_events = Event.where(:begin=> @start_date..@end_date).order("begin")
 
     @latest_info = Info.order("created_at DESC").first
 
     @next_sinf_concert = Concert.where("begin>=:begin AND orchestra=:orchestra",
-                          {:begin=>Date.today-1, :orchestra=>"Sinfonieorchester"}).order("begin").first
+                          {:begin=>Date.today, :orchestra=>"Sinfonieorchester"}).order("begin").first
     @next_skp_concert = Concert.where("begin>=:begin AND orchestra=:orchestra",
-                          {:begin=>Date.today-1, :orchestra=>"Stadtkapelle"}).order("begin").first
+                          {:begin=>Date.today, :orchestra=>"Stadtkapelle"}).order("begin").first
 
     respond_to do |format|
       format.html

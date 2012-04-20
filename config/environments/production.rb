4e97a03ef8d269dc4f4b379f4d28cf6a3cdcd8d6
Ralph Guderlei <ralph.guderlei@gmail.com>
2012-04-20 21:57:24 +0200

    use asset pipeline in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Mvb::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true

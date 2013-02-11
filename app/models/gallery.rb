69cf80cb9435a7cc1152ef7962e8b0c75f12705a
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-02-11 22:05:59 +0100

    bugfix: photos can be deleted from a gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,8 +1,8 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :name, :context_date, :photos, :photos_attributes
   has_many :photos, :dependent => :destroy
-  validates :context_date, :presence => true
-  validates :name, :length => { :in => 6..35 }
+  validates :name, :context_date, :presence => true
+  validates :name, :length => { :in => 4..35 }
 
-  accepts_nested_attributes_for :photos
+  accepts_nested_attributes_for :photos, :allow_destroy => true
 end

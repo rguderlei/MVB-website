bf5bbd9dce87091bb94b0be7e46f29502540d504
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-02-11 20:55:17 +0100

    fix layout galleries overview, restict length of gallery name to 35 chars

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,6 +2,7 @@ class Gallery < ActiveRecord::Base
   attr_accessible :name, :context_date, :photos, :photos_attributes
   has_many :photos, :dependent => :destroy
   validates :context_date, :presence => true
+  validates :name, :length => { :in => 6..35 }
 
   accepts_nested_attributes_for :photos
 end

48fef0dff3ae25405204172049964f8dd32de4b8
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-03-01 21:52:28 +0100

    add source ref for the page title stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   # used to print individual titles on each page
+  # see  https://gist.github.com/stammy/941997
   def yield_for(content_sym, default)
     output = content_for(content_sym)
     output = default if output.blank?

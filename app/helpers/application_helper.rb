29f38f6140958a66afd5479f34157f1dc24303e5
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-03-01 21:51:16 +0100

    add custom page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # used to print individual titles on each page
+  def yield_for(content_sym, default)
+    output = content_for(content_sym)
+    output = default if output.blank?
+    output = "MVB - " + output if !output.blank?
+    output
+  end
 end

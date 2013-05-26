8c9fea8491b06ce303c9899a6e8390cd39676c3a
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-05-26 15:16:18 +0200

    Layout Startseite optimiert, nächste Konzerte auf Startseite

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
   def index
-    today = Date.today
-    @start_date = Date.today  # Date.new(today.year, today.month, 1)
-    @end_date = Date.today + 30  # Date.new(today.year, today.month + 1, 1) -1
-    @monthly_events = Event.where(:start_at=> @start_date..@end_date).order("start_at")
+    @start_date = Date.today
+    @next_concerts = Concert.where("start_at > ?", @start_date).order("start_at")
 
     @latest_info = Info.order("created_at DESC").first
 

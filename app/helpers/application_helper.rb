3da4ced7bd278b47ec528c935e7606a0ae2c9e85
Ralph Guderlei <ralph.guderlei@gmail.com>
2014-01-10 20:50:05 +0100

    print location link only when location is set

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,13 @@ module ApplicationHelper
 
   def link_to_location(event_date)
 
-    href = "http://maps.google.de/maps?q=#{event_date.google_maps_query}"
-    output = "#{event_date.location_name} <a class=\"event-location noprint\" target=\"_blank\" title=\"\" data-placement=\"right\" data-toggle=\"tooltip\" href=\"#{href}\" data-original-title=\"#{event_date.location_tooltip}\"><i class=\"icon-globe\"></i></a>"
+    if !event_date.event_location.nil?
+      href = "http://maps.google.de/maps?q=#{event_date.google_maps_query}"
+      output = "#{event_date.location_name} <a class=\"event-location noprint\" target=\"_blank\" title=\"\" data-placement=\"right\" data-toggle=\"tooltip\" href=\"#{href}\" data-original-title=\"#{event_date.location_tooltip}\"><i class=\"icon-globe\"></i></a>"
 
-    output.html_safe
+      output.html_safe
+    else
+      ''
+    end
   end
 end

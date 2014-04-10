d6e89095afb4e6746f305007005b98c9ef736b81
Ralph Guderlei <ralph.guderlei@gmail.com>
2014-04-10 14:42:21 +0200

    fix paperclip content-type validation issue

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -4,6 +4,7 @@ class Concert < Event
   has_attached_file :image,  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
       :url => "/system/:attachment/:id/:style/:filename", :styles => {:small=>"90x90#", :medium => "210x150#", :large => "320x230#", :xlarge => "800x800"}
 
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 
   validates :title, :orchestra, :presence => true
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,6 @@ class Photo < ActiveRecord::Base
   has_attached_file :image, :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
       :url => "/system/:attachment/:id/:style/:filename", :styles => {:small=>"90x90#", :medium => "210x150#", :large => "320x230#", :xlarge => "800x800"}
   # validates_attachment_presence :image
+
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 end

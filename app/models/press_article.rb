b0a7c0e83bf16cceef25f7f4633d9a87c47d3a40
Ralph Guderlei <ralph.guderlei@gmail.com>
2014-04-10 14:50:08 +0200

    fix typo in validator

--- app/models/press_article.rb
+++ app/models/press_article.rb
@@ -4,7 +4,7 @@ class PressArticle < ActiveRecord::Base
   has_attached_file :article,  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
       :url => "/system/:attachment/:id/:style/:filename"
 
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
+  validates_attachment :article, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 
   validates :title, :print_date, :publisher, :presence => true
 end

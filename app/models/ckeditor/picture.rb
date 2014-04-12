90161a1f0d6788157e6b59f92508281a4c55faaa
Ralph Guderlei <ralph.guderlei@gmail.com>
2014-04-12 22:45:04 +0200

    fix paperclip issues in ckeditor models

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -6,6 +6,8 @@ class Ckeditor::Picture < Ckeditor::Asset
 	
 	validates_attachment_size :data, :less_than => 2.megabytes
 	validates_attachment_presence :data
+
+  validates_attachment :data, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 	
 	def url_content
 	  url(:content)

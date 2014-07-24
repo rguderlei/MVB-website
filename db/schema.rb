568e116b1b1ab98dd8d942f50d9571eeb52106fe
Ralph Guderlei <ralph.guderlei@gmail.com>
2014-07-24 21:38:49 +0200

    fix database migration for devise 2

--- db/schema.rb
+++ db/schema.rb
@@ -9,36 +9,36 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140104101615) do
+ActiveRecord::Schema.define(version: 20140104101615) do
 
-  create_table "ckeditor_assets", :force => true do |t|
-    t.string   "data_file_name",                  :null => false
+  create_table "ckeditor_assets", force: true do |t|
+    t.string   "data_file_name",               null: false
     t.string   "data_content_type"
     t.integer  "data_file_size"
     t.integer  "assetable_id"
-    t.string   "assetable_type",    :limit => 30
-    t.string   "type",              :limit => 30
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
-  create_table "event_dates", :force => true do |t|
+  create_table "event_dates", force: true do |t|
     t.datetime "start_at"
     t.datetime "end_at"
     t.string   "location"
     t.string   "additional_description"
     t.integer  "event_location_id"
     t.integer  "event_id"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "event_locations", :force => true do |t|
+  create_table "event_locations", force: true do |t|
     t.string   "short_name"
     t.string   "long_name"
     t.string   "street"
@@ -47,17 +47,15 @@ ActiveRecord::Schema.define(:version => 20140104101615) do
     t.string   "country"
     t.decimal  "lat"
     t.decimal  "long"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "events", :force => true do |t|
+  create_table "events", force: true do |t|
     t.string   "orchestra"
     t.string   "title"
     t.text     "description"
     t.boolean  "public_event"
-    t.text     "announcement"
-    t.text     "critique"
     t.string   "type"
     t.string   "image_file_name"
     t.string   "image_content_type"
@@ -65,39 +63,23 @@ ActiveRecord::Schema.define(:version => 20140104101615) do
     t.datetime "image_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "location_id"
-    t.integer  "alternate_event_id"
-    t.string   "alternate_event_type"
   end
 
-  add_index "events", ["alternate_event_id"], :name => "index_events_on_alternate_event_id"
-
-  create_table "galleries", :force => true do |t|
+  create_table "galleries", force: true do |t|
     t.string   "name"
     t.date     "context_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "infos", :force => true do |t|
+  create_table "infos", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "locations", :force => true do |t|
-    t.string   "name"
-    t.string   "street"
-    t.integer  "zipcode"
-    t.string   "city"
-    t.float    "lat"
-    t.float    "long"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "pages", :force => true do |t|
+  create_table "pages", force: true do |t|
     t.string   "name"
     t.string   "permalink"
     t.text     "main_content"
@@ -105,7 +87,7 @@ ActiveRecord::Schema.define(:version => 20140104101615) do
     t.datetime "updated_at"
   end
 
-  create_table "photos", :force => true do |t|
+  create_table "photos", force: true do |t|
     t.integer  "gallery_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -115,7 +97,7 @@ ActiveRecord::Schema.define(:version => 20140104101615) do
     t.datetime "image_updated_at"
   end
 
-  create_table "plans", :force => true do |t|
+  create_table "plans", force: true do |t|
     t.text     "orchestra"
     t.text     "header"
     t.text     "footer"
@@ -123,7 +105,7 @@ ActiveRecord::Schema.define(:version => 20140104101615) do
     t.datetime "updated_at"
   end
 
-  create_table "press_articles", :force => true do |t|
+  create_table "press_articles", force: true do |t|
     t.string   "title"
     t.date     "print_date"
     t.string   "publisher"
@@ -132,28 +114,28 @@ ActiveRecord::Schema.define(:version => 20140104101615) do
     t.string   "article_content_type"
     t.integer  "article_file_size"
     t.datetime "article_updated_at"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "",    :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.boolean  "admin",                  default: false
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "password_salt"
-    t.boolean  "admin",                                 :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end

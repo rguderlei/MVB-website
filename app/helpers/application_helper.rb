a0c2bd2596c1c5ca704cf1d8097815f4504b4c31
Ralph Guderlei <ralph.guderlei@gmail.com>
2015-05-26 21:46:45 +0200

    fix location icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
 
     if !event_date.event_location.nil?
       href = "http://maps.google.de/maps?q=#{event_date.google_maps_query}"
-      output = "#{event_date.location_name} <a class=\"event-location noprint\" target=\"_blank\" title=\"\" data-placement=\"right\" data-toggle=\"tooltip\" href=\"#{href}\" data-original-title=\"#{event_date.location_tooltip}\"><i class=\"icon-globe\"></i></a>"
+      output = "#{event_date.location_name} <a class=\"event-location noprint\" target=\"_blank\" title=\"\" data-placement=\"right\" data-toggle=\"tooltip\" href=\"#{href}\" data-original-title=\"#{event_date.location_tooltip}\"><i class=\"glyphicon glyphicon-globe\"></i></a>"
 
       output.html_safe
     else

fc10e7d7359d208f29fce58ea1e86a413fb42d40
Ralph Guderlei <ralph.guderlei@gmail.com>
2015-07-17 13:45:30 +0200

    add sponsor logos

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140104101615) do
 
-  create_table "ckeditor_assets", force: true do |t|
+  create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
     t.string   "data_content_type"
     t.integer  "data_file_size"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20140104101615) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
-  create_table "event_dates", force: true do |t|
+  create_table "event_dates", force: :cascade do |t|
     t.datetime "start_at"
     t.datetime "end_at"
     t.string   "location"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20140104101615) do
     t.datetime "updated_at"
   end
 
-  create_table "event_locations", force: true do |t|
+  create_table "event_locations", force: :cascade do |t|
     t.string   "short_name"
     t.string   "long_name"
     t.string   "street"
@@ -51,43 +51,43 @@ ActiveRecord::Schema.define(version: 20140104101615) do
     t.datetime "updated_at"
   end
 
-  create_table "events", force: true do |t|
-    t.string   "orchestra"
-    t.string   "title"
+  create_table "events", force: :cascade do |t|
+    t.string   "orchestra",          limit: 255
+    t.string   "title",              limit: 255
     t.text     "description"
     t.boolean  "public_event"
-    t.string   "type"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
+    t.string   "type",               limit: 255
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "galleries", force: true do |t|
-    t.string   "name"
+  create_table "galleries", force: :cascade do |t|
+    t.string   "name",         limit: 255
     t.date     "context_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "infos", force: true do |t|
-    t.string   "title"
+  create_table "infos", force: :cascade do |t|
+    t.string   "title",      limit: 255
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "pages", force: true do |t|
-    t.string   "name"
-    t.string   "permalink"
+  create_table "pages", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.string   "permalink",    limit: 255
     t.text     "main_content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "photos", force: true do |t|
+  create_table "photos", force: :cascade do |t|
     t.integer  "gallery_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 20140104101615) do
     t.datetime "image_updated_at"
   end
 
-  create_table "plans", force: true do |t|
+  create_table "plans", force: :cascade do |t|
     t.text     "orchestra"
     t.text     "header"
     t.text     "footer"
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 20140104101615) do
     t.datetime "updated_at"
   end
 
-  create_table "press_articles", force: true do |t|
+  create_table "press_articles", force: :cascade do |t|
     t.string   "title"
     t.date     "print_date"
     t.string   "publisher"
@@ -118,7 +118,7 @@ ActiveRecord::Schema.define(version: 20140104101615) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"

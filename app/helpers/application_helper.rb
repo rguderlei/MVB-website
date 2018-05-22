44f40a6bbd6afeaf1c877e1f91d03f344e22ba0f
rguderlei <ralph.guderlei@gmail.com>
2018-05-22 20:47:49 +0200

    gdpr: remove link to google maps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,14 +9,14 @@ module ApplicationHelper
   end
 
   def link_to_location(event_date)
-
-    if !event_date.event_location.nil?
-      href = "http://maps.google.de/maps?q=#{event_date.google_maps_query}"
-      output = "#{event_date.location_name} <a class=\"event-location noprint\" target=\"_blank\" title=\"\" data-placement=\"right\" data-toggle=\"tooltip\" href=\"#{href}\" data-original-title=\"#{event_date.location_tooltip}\"><i class=\"glyphicon glyphicon-globe\"></i></a>"
-
-      output.html_safe
-    else
-      ''
-    end
+    ''
+    #if !event_date.event_location.nil?
+    #  href = "http://maps.google.de/maps?q=#{event_date.google_maps_query}"
+    #  output = "#{event_date.location_name} <a class=\"event-location noprint\" target=\"_blank\" title=\"\" data-placement=\"right\" data-toggle=\"tooltip\" href=\"#{href}\" data-original-title=\"#{event_date.location_tooltip}\"><i class=\"glyphicon glyphicon-globe\"></i></a>"
+#
+#      output.html_safe
+#    else
+#      ''
+#    end
   end
 end
